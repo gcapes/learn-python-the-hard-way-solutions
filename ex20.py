c32f67f838e64d1d991bd62a8cdc566300acbc1f
Gerard Capes <gerardcapes@gmail.com>
2016-10-06 15:26:55 +0100

    Add ex20 study drill 1 - add comments

--- ex20.py
+++ ex20.py
@@ -1,20 +1,27 @@
+# import command line argument function from sys module
 from sys import argv
 
+# Assign command line arguments to variables
 script, input_file = argv
 
+# Function to read file and print it
 def print_all(f):
-        print f.read()
+    print f.read()
 
+# Rewind file to beginning
 def rewind(f):
-        f.seek(0)
+    f.seek(0)
 
+# Print current line from the file
 def print_a_line(line_count, f):
-        print line_count, f.readline()
+    print line_count, f.readline()
 
+# Create local variable for input_file
 current_file = open(input_file)
 
 print "First let's print the whole file:\n"
 
+# Call print all function
 print_all(current_file)
 
 print "Now let's rewind, kind of like a tape."
@@ -24,10 +31,15 @@ rewind(current_file)
 print "Let's print three lines:"
 
 current_line = 1
+# Print current line
 print_a_line(current_line, current_file)
 
+# Increment which line to print
 current_line = current_line + 1
+# Print current line
 print_a_line(current_line, current_file)
 
+# Increment which line to print
 current_line = current_line + 1
+# Print current line
 print_a_line(current_line, current_file)
